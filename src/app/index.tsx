c320e3b93fefd5e534a76d9d4482016dee8141b2
andressaaborges <emylleaborges@gmail.com>
2024-02-12 14:57:16 -0300

    feat: :tada: first commit

---
+++ src/app/index.tsx
@@ -0,0 +1,11 @@
+import { Header } from "@/components/header";
+import { View, Text } from "react-native";
+
+
+export default function Home() {
+    return (
+        <View className="pt-8 flex-1">
+            <Header title="Pedido"/>
+        </View>
+    )
+}
