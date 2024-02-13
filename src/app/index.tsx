acdd5915344a7f4204de507cd2cbaaadb385aca3
andressaaborges <emylleaborges@gmail.com>
2024-02-13 18:26:44 -0300

    feat: :sparkles: create products listing with links using expo-router

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,14 +1,26 @@
 import { CategoryButton } from "@/components/category-button";
 import { Header } from "@/components/header";
-import { View, FlatList } from "react-native";
-import { CATEGORIES } from "@/utils/data/products";
-import { useState } from "react";
+import { View, Text, FlatList, SectionList } from "react-native";
+import { CATEGORIES, MENU } from "@/utils/data/products";
+import { useState, useRef } from "react";
+import { Product } from "@/components/product";
+import { Link } from "expo-router";
 
 export default function Home() {
     const [category, setCategory] = useState(CATEGORIES[0])
+    const sectionListRef = useRef<SectionList>(null)
 
     function handleCategorySelect(SelectedCategory: string) {
         setCategory(SelectedCategory)
+        const sectionIndex = CATEGORIES.findIndex((category) => category === SelectedCategory)
+
+        if (sectionListRef) {
+            sectionListRef.current?.scrollToLocation({
+                animated: true,
+                sectionIndex,
+                itemIndex: 0,
+            })
+        }
     }
 
     return (
@@ -22,6 +34,25 @@ export default function Home() {
                 className="max-h-10 mt-5"
                 showsHorizontalScrollIndicator={false}
                 contentContainerStyle={{ gap: 12, paddingHorizontal: 20 }}
+            />
+
+            <SectionList
+                ref={sectionListRef}
+                sections={MENU}
+                keyExtractor={(item) => item.id}
+                stickySectionHeadersEnabled={false}
+                renderItem={({ item }) => (
+                    <Link href={`/product/${item.id}`} asChild>
+                        <Product data={item} />
+                    </Link>)}
+                renderSectionHeader={({ section: { title } }) => (
+                    <Text className="text-xl text-white font-heading mt-8 mb-3">
+                        {title}
+                    </Text>
+                )}
+                className="flex-1 p-5"
+                showsVerticalScrollIndicator={false}
+                contentContainerStyle={{ paddingBottom: 100 }}
             />
         </View>
     )
---
+++ src/app/product/[id].tsx
@@ -0,0 +1,13 @@
+import { View } from "react-native";
+import { useLocalSearchParams } from "expo-router";
+
+export default function Product() {
+    const { id } = useLocalSearchParams()
+
+
+    return (
+        <View className="flex-1">
+
+        </View>
+    )
+}
