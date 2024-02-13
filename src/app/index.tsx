cd6359fabc2653591d83378d10605e85da06ed03
andressaaborges <emylleaborges@gmail.com>
2024-02-13 15:59:16 -0300

    feat: :sparkles: add `category` component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,28 @@
+import { CategoryButton } from "@/components/category-button";
 import { Header } from "@/components/header";
-import { View } from "react-native";
-
+import { View, FlatList } from "react-native";
+import { CATEGORIES } from "@/utils/data/products";
+import { useState } from "react";
 
 export default function Home() {
+    const [category, setCategory] = useState(CATEGORIES[0])
+
+    function handleCategorySelect(SelectedCategory: string) {
+        setCategory(SelectedCategory)
+    }
+
     return (
         <View className="pt-8 flex-1">
             <Header title="Faça seu Pedido" cartQuantityItems={4} />
+            <FlatList
+                data={CATEGORIES}
+                keyExtractor={(item) => item}
+                renderItem={({ item }) => (<CategoryButton title={item} isSelected={item === category} onPress={() => handleCategorySelect(item)} />)}
+                horizontal
+                className="mah-h-10 mt-5"
+                showsHorizontalScrollIndicator={false}
+                contentContainerStyle={{ gap: 12, paddingHorizontal: 20 }}
+            />
         </View>
     )
 }
